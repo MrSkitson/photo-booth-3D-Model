a59f6e8a9d32816234a558cb8d29646d1206d60c
MrSkit <91326600+MrSkitson@users.noreply.github.com>
2022-03-17 16:51:44 +0100

    Add Button
    Add UI Button

--- My project/Assets/Scripts/SpawnManager.cs
+++ My project/Assets/Scripts/SpawnManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    
     public GameObject Cube;
     public GameObject Capsule;
     public GameObject Sphere;
@@ -18,7 +19,6 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         
         //InvokeRepeating("CubeSpown", startDelay, modelSpawnTime);
         CubeSpown();
     }
